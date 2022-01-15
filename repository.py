e0ced2c32a4de73d07d39dd7aa8663678971d055
Bartolomé Sánchez Salado <bartolome.salado@kiwi.com>
2022-01-15 18:56:02 +0100

    Add SQLALchemy repository

---
+++ repository.py
@@ -0,0 +1,26 @@
+import abc
+
+from sqlalchemy.orm.session import Session
+
+import model
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> model.Batch:
+        raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, batch: model.Batch):
+        self.session.add(batch)
+
+    def get(self, reference: str) -> model.Batch:
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
